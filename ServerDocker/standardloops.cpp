41cd1b1358a195c92c93a53c52a1aa09e39accd9
MawWebby <mawhitworth01@gmail.com>
2024-12-16 23:10:31 -0600

    Update Log to Output to File

--- ServerDocker/standardloops.cpp
+++ ServerDocker/standardloops.cpp
@@ -2,6 +2,13 @@
 #include "globalvariables.h"
 
 
+std::string charactermap = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+int serverStatus;
+int updating;
+int stopping;
+
+// LOGFILE
+const char* logfile = "/home/serverdump/log.txt";
 
 
 //////////////////////
@@ -23,11 +30,43 @@ std::string timedetector() {
 // Send to Logger Scripts //
 ////////////////////////////
 void sendtolog(std::string data2) {
-    std::cout << data2 << std::endl;
+    serverStatus = serverStarted.load();
+    updating = updateSIGNAL.load();
+    stopping = stopSIGNAL.load();
+
+    if (serverStatus == false || updating == true || stopping == true) {
+        std::cout << data2 << std::endl;
+    } 
+    
+    std::ofstream logfilestream;
+    logfilestream.open(logfile, std::ofstream::out | std::ofstream::app);
+    if (logfilestream.is_open() == true) {
+        logfilestream << data2 << std::endl;
+    } else {
+        std::cout << "[ERRO] - COULD NOT SAVE TO LOGFILE!" << std::endl;
+        std::cout << "[ERRO] - MESSAGE: " << data2 << std::endl;
+    }
+    logfilestream.close();
 }
 
 void sendtologopen(std::string data2) {
-    std::cout << data2;
+    serverStatus = serverStarted.load();
+    updating = updateSIGNAL.load();
+    stopping = stopSIGNAL.load(); 
+
+    if (serverStatus == false || updating == true || stopping == true) {
+        std::cout << data2;
+    } 
+    
+    std::ofstream logfilestream;
+    logfilestream.open(logfile, std::ofstream::out | std::ofstream::app);
+    if (logfilestream.is_open() == true) {
+        logfilestream << data2;
+    } else {
+        std::cout << "[ERRO] - COULD NOT SAVE TO LOGFILE!" << std::endl;
+        std::cout << "[ERRO] - MESSAGE: " << data2 << std::endl;
+    }
+    logfilestream.close();
 }
 
 void logdebug(std::string data2, bool complete) {
@@ -75,6 +114,29 @@ void logerror(std::string headerdata2, std::string errormessage) {
     std::string data2 = "[ERROR] - " + timedet + " - " + headerdata2 + " - " + errormessage;
     sendtolog(data2);
 }
+
+
+
+
+/////////////////////////////////
+//// READ FROM LOGGER SRIPTS ////
+/////////////////////////////////
+void readfromlogger() {
+    std::ifstream logfilestream;
+    logfilestream.open(logfile);
+    if (logfilestream.is_open() == true) {
+        char linebits[2048];
+        while (logfilestream.eof() == false) {
+            logfilestream.getline(linebits, 2048);
+            std::cout << linebits << std::endl;
+        }
+        std::cout << std::endl << "Reached End of Log File" << std::endl;
+    } else {
+        std::cout << "[ERRO] - COULD NOT OPEN LOGFILE!" << std::endl;
+    }
+    logfilestream.close();
+}
+
 
 
 
@@ -167,4 +229,99 @@ int stringtoint(std::string values) {
     }
 
     return valuesdetermine;
+}
+
+
+
+
+/////////////////////////////////////
+//// GENERATE API RANDOM STRINGS ////
+/////////////////////////////////////
+std::string generateRandomStringHoneyPI() {
+
+    // Define the list of possible characters
+    const std::string CHARACTERS = charactermap;
+
+    // Create a random number generator
+    std::random_device rd;
+    std::mt19937 generator(rd());
+
+    // Create a distribution to uniformly select from all
+    // characters
+    std::uniform_int_distribution<> distribution(0, CHARACTERS.size() - 1);
+
+    // Generate the random string
+    std::string random_string = "PI";
+    for (int i = 0; i < 62; ++i) {
+        random_string += CHARACTERS[distribution(generator)];
+    }
+
+    return random_string;
+}
+
+std::string generateRandomStringRouterAPI() {
+
+    // Define the list of possible characters
+    const std::string CHARACTERS = charactermap;
+
+    // Create a random number generator
+    std::random_device rd;
+    std::mt19937 generator(rd());
+
+    // Create a distribution to uniformly select from all
+    // characters
+    std::uniform_int_distribution<> distribution(0, CHARACTERS.size() - 1);
+
+    // Generate the random string
+    std::string random_string = "RO";
+    for (int i = 0; i < 62; ++i) {
+        random_string += CHARACTERS[distribution(generator)];
+    }
+
+    return random_string;
+}
+
+std::string generateRandomFileName() {
+
+    timedetector();
+
+    // Define the list of possible characters
+    const std::string CHARACTERS = charactermap;
+
+    // Create a random number generator
+    std::random_device rd;
+    std::mt19937 generator(rd());
+
+    // Create a distribution to uniformly select from all
+    // characters
+    std::uniform_int_distribution<> distribution(0, CHARACTERS.size() - 1);
+
+    // Generate the random string
+    std::string random_string = std::to_string(currenthour) + "_" + std::to_string(currentdays) + "_" + std::to_string(currentyear);
+    for (int i = 0; i < 6; ++i) {
+        random_string += CHARACTERS[distribution(generator)];
+    }
+
+    return random_string;
+}
+
+std::string generateRandomClientKey() {
+    // Define the list of possible characters
+    const std::string CHARACTERS = charactermap;
+
+    // Create a random number generator
+    std::random_device rd;
+    std::mt19937 generator(rd());
+
+    // Create a distribution to uniformly select from all
+    // characters
+    std::uniform_int_distribution<> distribution(0, CHARACTERS.size() - 1);
+
+    // Generate the random string
+    std::string random_string = "SS";
+    for (int i = 0; i < 62; ++i) {
+        random_string += CHARACTERS[distribution(generator)];
+    }
+
+    return random_string;
 }
