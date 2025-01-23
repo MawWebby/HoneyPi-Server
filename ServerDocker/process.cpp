3a5a7d3398279810325ada8477f282c6cdd718b6
MawWebby <mawhitworth01@gmail.com>
2025-01-23 10:00:21 -0600

    It Works!

--- ServerDocker/process.cpp
+++ ServerDocker/process.cpp
@@ -2,8 +2,14 @@
 #include "globalvariables.h"
 
 // MAP VARIABLES
-std::map<std::string, std::string> userpasscombo;
+std::map<int, std::string> usercombomap;
+std::map<int, std::string> passcombomap;
 std::map<int, std::string> commandmap;
+std::map<int, std::string> filesmap;
+std::map<int, std::string> ipaddrmap;
+std::map<int, std::string> extramap;
+std::map<int, std::string> filechangesmap;
+std::map<int, std::string> fileeditsmap;
 
 // GENERAL VARIABLES
 int reportV = 0;
@@ -44,32 +50,55 @@ std::string ipaddress = "";
 //////////////////////////////////////////////
 int determineseverity() {
 
+    return -1;
 }
 
 
-////////////////////////////////////////////////////
-////// PROCESS REPORT FOR THE COMMAND TO WORK //////
-////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////
+////// PROCESS REPORT FOR THE COMMAND TO WORK (MAIN LOOP) //////
+////////////////////////////////////////////////////////////////
 
 // RETURN 10 - FILE STREAM NOT OPENED CORRECTLY
+// RETURN 11 - READ/WRITE ERROR
+// RETURN 12 - LOGICAL IO ERROR
 // RETURN 254 - NOT VALID FILENAME
 // RETURN 255 - SHOULD NEVER REACH HERE
 int processReport(std::string filename) {
 
+    int linenumber = 0;
+
     // CHECK FOR VALID PARAMETERS
     if (filename == "") {
-        logwarning("Log File Not Found: ", false);
+        logwarning("Log File NULL: ", false);
         sendtolog(filename);
         return 254;
         return 254;
     }
 
+    if (filename == "/home/testreport.txt") {
+        std::cout << "DEBUG MODE" << std::endl;
+    }
+
     // OPEN INPUT FILE
     std::ifstream reportstream;
     reportstream.open(filename);
-    if (reportstream.is_open() != 0) {
+    sleep(1);
+
+    if (reportstream.is_open() != 1) {
         logcritical("UNABLE TO OPEN INPUT FILE STREAM: ", false);
         sendtolog(filename);
+        if (reportstream.bad() == true) {
+            logcritical("Received Read/Write Error!", true);
+            return 11;
+            return 11;
+        } else if (reportstream.fail() == true) {
+            logcritical("Received Logical IO Error!", true);
+            return 12;
+            return 12;
+        } else {
+            return 10;
+        }
         return 10;
         return 10;
     }
@@ -82,16 +111,35 @@ int processReport(std::string filename) {
     bool ipaddr = false;
     bool extraopt = false;
     bool filechanges = false;
-    char lineraw[2048] = "";
+    bool fileedits = false;
+    char lineraw[2048];
+
+    // INFORMATION OF REPORTS
+    int commandsranonreport = 0;
+    int filestranonreport = 0;
+    int ipaddressesonreport = 0;
+    int extraoptonreport = 0;
+    int fileschangeonreport = 0;
+    int fileeditsonreport = 0;
+    int useronreport = 0;
+    int passonreport = 0;
 
 
     while (completionproc != true || reportstream.eof() != true) {
-        reportstream.getline(lineraw, 2048);
+        reportstream.getline(lineraw, 300);
+
+        // CONVERT TO STRING FOR ANALYSIS
         std::string linestr = lineraw;
+        
+        // DEBUG HANDLER
+        if (filename == "/home/testreport.txt") {
+            std::cout << linenumber << ": " << linestr.length() << ": " << lineraw << std::endl;
+            linenumber = linenumber + 1;
+        }
 
         // MAKE SURE IT IS NOT BLANK
         if (linestr.length() > 2) {
-            if (usercombo == false || commandprocess == false || files == false || ipaddr == false || extraopt == false || filechanges == false) {
+            if (usercombo == false && commandprocess == false && files == false && ipaddr == false && extraopt == false && filechanges == false && fileedits == false) {
                 // ANALYZE EACH STRING
                 std::string matchcondition = linestr.substr(0,2);
                 if (matchcondition == "//") {
@@ -182,14 +230,6 @@ int processReport(std::string filename) {
                             }
                         }
                     }
-                } else if (matchcondition == "fr") {
-
-                    // fromIP - IP ADDRESS REPORT IS FROM
-                    if (linestr.length() > 9) {
-                        if (linestr.substr(6,0) == "fromIP") {
-                            ipaddress = linestr.substr(linestr.length() - 10, linestr.length() - 11);
-                        }
-                    }
                 } else if (matchcondition == "pa") {
                     
                     // partofDDOS - Part of DDOS = False
@@ -217,8 +257,9 @@ int processReport(std::string filename) {
 
                     // commandProcess - TERMINAL PROCESS
                     if (linestr.length() == 18) {
-                        if (linestr.substr(18,0) == "commandprocess = {") {
+                        if (linestr.substr(0,18) == "commandprocess = {") {
                             commandprocess = true;
+                            std::cout << "REACHED HERE" << std::endl;
                         }
                     } 
                 } else if (matchcondition == "op") {
@@ -295,7 +336,7 @@ int processReport(std::string filename) {
                     
                     // usercombo - PASSWORD BRUTE FORCE
                     if (linestr.length() == 13) {
-                        if (linestr.substr(13,0) == "usercombo = {") {
+                        if (linestr.substr(0,13) == "usercombo = {") {
                             usercombo = true;
                         }
                     }
@@ -303,14 +344,21 @@ int processReport(std::string filename) {
 
                     // files - FILES INTERACTED          
                     if (linestr.length() == 9) {
-                        if (linestr.substr(9,0) == "files = {") {
+                        if (linestr.substr(0,9) == "files = {") {
                             files = true;
                         }
                     }  
 
                     // filechanges - FILES INTERACTED          
                     if (linestr.length() == 15) {
-                        if (linestr.substr(15,0) == "filechanges = {") {
+                        if (linestr.substr(0,15) == "filechanges = {") {
+                            filechanges = true;
+                        }
+                    }  
+
+                    // fileedits - FILES EDITED          
+                    if (linestr.length() == 13) {
+                        if (linestr.substr(0,13) == "fileedits = {") {
                             filechanges = true;
                         }
                     }  
@@ -318,7 +366,7 @@ int processReport(std::string filename) {
                     
                     // ipaddr - IP ADDRESSES INTERACTED       
                     if (linestr.length() == 10) {
-                        if (linestr.substr(10,0) == "ipaddr = {") {
+                        if (linestr.substr(0,10) == "ipaddr = {") {
                             ipaddr = true;
                         }
                     }  
@@ -326,7 +374,7 @@ int processReport(std::string filename) {
                     
                     // extraopt - IP ADDRESSES INTERACTED       
                     if (linestr.length() == 12) {
-                        if (linestr.substr(12,0) == "extraopt = {") {
+                        if (linestr.substr(0,12) == "extraopt = {") {
                             extraopt = true;
                         }
                     }  
@@ -334,23 +382,70 @@ int processReport(std::string filename) {
                     completionproc = true;
                 }
             } else {
+                std::cout << "MAPPING" << std::endl;
                 if (usercombo == true) {
                     // USERNAME AND PASSWORD STRINGS TO MAP FACE
+                    if (linestr.length() >= 8 && linestr.substr(linestr.length() - 1, 1) == ")") {
+                        bool userpasscomplete = false;
+                        int currentchar = 0;
+                        int previousstart = 1;
+                        std::string usernameacc = "";
+                        std::string passwordacc = "";
+                        while (userpasscomplete == false) {
+                            if (linestr.length() >= currentchar + 1) {
+                                std::string currentstring = linestr.substr(currentchar, 1);
+                                if (currentstring == "(") {
+                                    // IGNORE
+                                } else if (currentstring == ";" && linestr.length() >= currentchar + 5) {
+                                    if (linestr.substr(currentchar, 5) == ";[$]:") {
+                                        usernameacc = linestr.substr(previousstart, currentchar - previousstart - 1);
+                                        previousstart = currentchar + 4;
+                                        currentchar = currentchar + 1;
+                                    }
+                                } else if (currentstring == ")") {
+                                    passwordacc = linestr.substr(previousstart, currentchar - previousstart - 1);
+                                    userpasscomplete = true;
+                                }
+                            }
+                            currentchar = currentchar + 1;
+                        }
+
+                        // LOG TO HEADER
+                        if (usernameacc != "" && passwordacc != "") {
+                            usercombomap[useronreport] = usernameacc;
+                            passcombomap[passonreport] = passwordacc;
+                            useronreport = useronreport + 1;
+                            passonreport = passonreport + 1;
+                        } else {
+                            logwarning("Received Invalid Combination of User/Pass - ", false);
+                            sendtolog(linestr);
+                        }
+
+                    }
                 } else if (commandprocess == true) {
                     std::string commandexec = linestr.substr(1, linestr.length() - 2);
-                    // COMMAND TO ADD IT TO THE MAP ABOVE
+                    commandmap.insert({commandsranonreport, commandexec});
+                    commandsranonreport = commandsranonreport + 1;
                 } else if (files == true) {
-                    std::string fileexec = linestr.substr(1, linestr.length() - 2);
-                    // COMMAND TO ADD IT TO MAP HERE
+                    std::string fileexec = linestr.substr(1, linestr.length());
+                    filesmap.insert({filestranonreport, fileexec});
+                    filestranonreport = filestranonreport + 1;
                 } else if (ipaddr == true) {
                     std::string ipaddr = linestr;
-                    // COMMAND TO MAP
+                    ipaddrmap[ipaddressesonreport] = ipaddr;
+                    ipaddressesonreport = ipaddressesonreport + 1;
                 } else if (extraopt == true) {
                     std::string extraexec = linestr.substr(1, linestr.length() - 2);
-                    // COMMAND TO MAP
+                    extramap[extraoptonreport] = extraexec;
+                    extraoptonreport = extraoptonreport + 1;
                 } else if (filechanges == true) {
-                    // FILE CHANGES
-
+                    std::string filechange = linestr.substr(1, linestr.length() - 2);
+                    filechangesmap[fileschangeonreport] = filechange;
+                    fileschangeonreport = fileschangeonreport + 1;
+                } else if (fileedits == true) {
+                    std::string fileedits = linestr.substr(1, linestr.length() - 2);
+                    fileeditsmap[fileeditsonreport] = fileedits;
+                    fileeditsonreport = fileeditsonreport + 1;
                 } else {
                     usercombo = false;
                     commandprocess = false;
@@ -358,6 +453,7 @@ int processReport(std::string filename) {
                     ipaddr = false;
                     extraopt = false;
                     filechanges = false;
+                    fileedits = false;
                 }
             }
         } else if (linestr.length() == 1) {
@@ -368,10 +464,52 @@ int processReport(std::string filename) {
                 ipaddr = false;
                 extraopt = false;
                 filechanges = false;
+                fileedits = false;
             }
         }
     }
 
+
+    // PRINT EVERYTHING TO CONSOLE IF DEBUG TEST REPORT IS USED
+    if (filename == "/home/testreport.txt") {
+        std::cout << "USER" << std::endl;
+        for (const auto& pair : usercombomap) {
+            std::cout << "#: " << pair.first << ", USER: " << pair.second << std::endl;
+        }
+        std::cout << "PASS" << std::endl;
+        for (const auto& pair : passcombomap) {
+            std::cout << "#: " << pair.first << ", PASS: " << pair.second << std::endl;
+        }
+        std::cout << "COMMAND" << std::endl;
+        for (const auto& pair : commandmap) {
+            std::cout << "#: " << pair.first << ", CMD: " << pair.second << std::endl;
+        }
+        std::cout << "FILES" << std::endl;
+        for (const auto& pair : filesmap) {
+            std::cout << "#: " << pair.first << ", FLS: " << pair.second << std::endl;
+        }
+        std::cout << "IPADDR" << std::endl;
+        for (const auto& pair : ipaddrmap) {
+            std::cout << "#: " << pair.first << ", IP: " << pair.second << std::endl;
+        }
+        std::cout << "EXTRAMAP" << std::endl;
+        for (const auto& pair : extramap) {
+            std::cout << "#: " << pair.first << ", EXTRA: " << pair.second << std::endl;
+        }
+        std::cout << "FILES CHANGED" << std::endl;
+        for (const auto& pair : filechangesmap) {
+            std::cout << "#: " << pair.first << ", FLSCHANGE: " << pair.second << std::endl;
+        }
+        std::cout << "FILEEDITSMAP" << std::endl;
+        for (const auto& pair : fileeditsmap) {
+            std::cout << "#: " << pair.first << ", FLSEDIT: " << pair.second << std::endl;
+        }
+    }
+    
+
+
+
+// THIS NEXT
 
     // do something with this
     return 255;
