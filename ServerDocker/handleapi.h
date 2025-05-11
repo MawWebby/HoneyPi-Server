ea7f29599f857f82507227c1d8b6adafa4d45a8f
MawWebby <mawhitworth01@gmail.com>
2025-05-11 12:52:30 -0500

    Update Header File for More Parameters

--- ServerDocker/handleapi.h
+++ ServerDocker/handleapi.h
@@ -12,11 +12,11 @@ int processAPI(int clientID, std::string header1, std::string data1, std::string
                std::string header4, std::string data4, std::string header5, 
                std::string data5, std::string header6, std::string data6, 
                std::string header7, std::string data7, std::string header8, 
-               std::string data8, std::string header9, std::string data9);
+               std::string data8, std::string header9, std::string data9, std::string clientIPADDR);
 
-int analyzeAPIandexecute(int clientID, std::string messageA);
+int analyzeAPIandexecute(int clientID, std::string messageA, std::string clientIPADDR);
 
-void apiconnectionthread(int clientID, std::string a11829, std::string b11829, std::string c11829);
+void apiconnectionthread(int clientID, std::string, std::string, std::string);
 
 void handle11829Connections(int server_fd4);
 
